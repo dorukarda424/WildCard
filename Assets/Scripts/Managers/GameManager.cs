df0b6150f7ec0183909325fbbcd0977ad8511421
Doruk Arda Ceviz <424arda@gmail.com>
2026-02-03 11:27:03 +0000

    Initial commit

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+
+    public string loggedInPlayerName;
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+
+            Destroy(gameObject);
+        }
+    }
+}
